28305270f2251f6743a79920b08c7199ca540b6a
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-06-10 15:59:26 +1200

    Add three command line options for changing the build process.

--- libcellml-dev/libcellml_build.sh
+++ libcellml-dev/libcellml_build.sh
@@ -2,12 +2,28 @@
 
 HELP=0
 POSITIONAL=()
+SERIAL_BUILD=FALSE
+BUILD_ONLY=FALSE
+DEBUG=FALSE
+
 while [[ $# -gt 0 ]]; do
   key="$1"
 
   case $key in
     -h|--help)
     HELP=1
+    shift # past argument
+    ;;
+    -s|--serial)
+    SERIAL_BUILD=TRUE
+    shift # past argument
+    ;;
+    -b|--build-only)
+    BUILD_ONLY=TRUE
+    shift # past argument
+    ;;
+    -d|--debug)
+    DEBUG=TRUE
     shift # past argument
     ;;
 #    -s|--searchpath)
@@ -35,6 +51,11 @@ BRANCH=$2
 BUILD_DIR=build-libcellml-release
 SRC_DIR=libcellml
 
+BUILD_ARGS=()
+if [ "x$SERIAL_BUILD" == "xFALSE" ]; then
+  BUILD_ARGS+=("-j")
+fi
+
 if [ -f "$REPOSITORY/CMakeLists.txt" ]; then
   echo "Using local repository."
   SRC_DIR=$REPOSITORY
@@ -52,23 +73,27 @@ if [ "$?" == "0" ]; then
   mkdir $BUILD_DIR
   cd $BUILD_DIR
   if cmake -DPREFERRED_CLANG_FORMAT_NAMES=clang-format-8 -DPREFERRED_CLANG_TIDY_NAMES=clang-tidy-8 $SRC_DIR; then
-    if make -j; then
-      if make test; then
-        if make coverage; then
-          if make memcheck; then
-            if make test_clang_format; then
-              echo "The libCellML repository passed all tests."
+    if make ${BUILD_ARGS[@]}; then
+      if [ "x$BUILD_ONLY" == "xTRUE" ]; then
+        echo "Finished building libraries and tests."
+      else
+        if make test; then
+          if make coverage; then
+            if make memcheck; then
+              if make test_clang_format; then
+                echo "The libCellML repository passed all tests."
+              else
+                echo "libCellML clang-format test failed."
+              fi
             else
-              echo "libCellML clang-format test failed."
+              echo "libCellML memory check test failed."
             fi
           else
-            echo "libCellML memory check test failed."
+            echo "libCellML code coverage test failed."
           fi
         else
-          echo "libCellML code coverage test failed."
+          echo "libCellML failed to pass all unit tests."
         fi
-      else
-        echo "libCellML failed to pass all unit tests."
       fi
     else
       echo "libCellML failed to compile."
@@ -78,5 +103,9 @@ if [ "$?" == "0" ]; then
   fi
 else
   echo "Git clone of '$REPOSITORY' using branch '$BRANCH' has failed."
+fi
+
+if [ "x$DEBUG" == "xTRUE" ]; then
+  exec /bin/bash
 fi
 
